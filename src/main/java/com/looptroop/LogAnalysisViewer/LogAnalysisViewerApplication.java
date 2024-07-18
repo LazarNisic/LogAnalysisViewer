859c54d3b75f2dc231fbbca08ea284176e236cdb
Nisic, Lazar <Lazar.Nisic@ncratleos.com>
2024-07-18 10:27:45 +0200

    test

---
+++ src/main/java/com/looptroop/LogAnalysisViewer/LogAnalysisViewerApplication.java
@@ -0,0 +1,13 @@
+package com.looptroop.LogAnalysisViewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnalysisViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAnalysisViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/looptroop/LogAnalysisViewer/LogAnalysisViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.looptroop.LogAnalysisViewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnalysisViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
